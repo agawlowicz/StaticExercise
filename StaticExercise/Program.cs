615d477c038fd921db4021dfd1f2a61845106b1c
Amanda Gawlowicz <agawlowicz07@gmail.com>
2022-04-01 10:11:05 -0500

    convert temperatures from user input, practice with static keyword in classes

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isTrue;
+            double temperature;
+            do
+            {
+                Console.WriteLine("Enter a Fahrenheit temperature to convert to degrees Celsius: ");
+                isTrue = double.TryParse(Console.ReadLine(), out temperature);
+            } while (!isTrue);
+
+            Console.WriteLine($"\n{temperature} degrees Fahrenheit = " +
+                $"{TempConverter.FahrenheitToCelsius(temperature)} degrees Celsius.\n");
+            Console.WriteLine("-------------------------------");
+
+            do
+            {
+                Console.WriteLine("Enter a Celsius temperature to convert to degrees Fahrenheit: ");
+                isTrue = double.TryParse(Console.ReadLine(), out temperature);
+            } while (!isTrue);
+
+            Console.WriteLine($"\n{temperature} degrees Celsius = " +
+                $"{TempConverter.CelsiusToFahrenheit(temperature)} degrees Fahrenheit.\n");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        => (5 / 9) * (fahrenheit - 32);
+
+        public static double CelsiusToFahrenheit(double celsius)
+        => (9 / 5) * celsius + 32;
+    }
+}
