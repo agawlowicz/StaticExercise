9ce8ee71850188d6be4d40419ea51203e7ba4044
Amanda Gawlowicz <agawlowicz07@gmail.com>
2022-04-01 11:41:27 -0500

    fixed rounding errors

--- StaticExercise/TempConverter.cs
+++ StaticExercise/TempConverter.cs
@@ -4,9 +4,9 @@ namespace StaticExercise
     public static class TempConverter
     {
         public static double FahrenheitToCelsius(double fahrenheit)
-        => (5 / 9) * (fahrenheit - 32);
+        => (5.0 / 9.0) * (fahrenheit - 32); //use double for 5 and 9 so there are no rounding errors
 
         public static double CelsiusToFahrenheit(double celsius)
-        => (9 / 5) * celsius + 32;
+        => (9.0 / 5.0) * celsius + 32;
     }
 }
